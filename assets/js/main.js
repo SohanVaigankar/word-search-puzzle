e043c0996d9109db8af2ed788e3862588bd9a8f5
Sohan <svaigankar2000@gmail.com>
2021-06-13 00:56:39 +0530

    make progress bar run w.r.t time

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
 import { keywords } from "./keywordList.js";
 
+var progressBar = document.querySelector(".progress-bar");
+
 console.log(keywords);
+
+// Progress bar
+function progressBarFun(current_progress, timeLimit) {
+  var interval = setInterval(function () {
+    current_progress += 1 / timeLimit;
+    console.log(current_progress);
+    progressBar.setAttribute("style", `width:${current_progress}%`);
+    progressBar.setAttribute("aria-valuenow", current_progress);
+    if (current_progress >= 100) clearInterval(interval);
+  }, 10);
+}
+
+var current_progress = 0;
+var timeLimit = 60;
+progressBarFun(current_progress, timeLimit);
+
+
+// TODO: 
