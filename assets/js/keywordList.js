5d474a7425904c5e543aa71a8ccaeed86ffa5e8e
Sohan <svaigankar2000@gmail.com>
2021-06-30 01:37:52 +0530

    add: story, The Golden Egg

--- assets/js/keywordList.js
+++ assets/js/keywordList.js
@@ -736,22 +736,20 @@ export const keywords = [
 
 export const story = [
   {
+    title: `The Golden  Egg`,
     part: [
       {
-        content: `Story 1 part 1 word1 word2 word3 word4`,
-        words: ["word1", "word2", "word3", "word4"],
+        content: `The computer reads us a story.. Once upon a time, a farmer had a goose that gives output as a golden egg every day. The egg provided enough money for the farmer and his wife for their day-to-day needs. The farmer and his wife were happy for a long time. But one day, the loop break. The farmer got an idea and thought, “Why should I take just one egg a day? Why can’t I execute the laying egg’s process at once and make a lot of money?”`,
+        words: ["computer", "output", "loop", "break", "execute", "process"],
       },
       {
-        content: `Story 1 part 2 word5 word6 word7 word8`,
-        words: ["word5", "word6", "word7", "word8"],
-      },
-      {
-        content: `Story 1 part 3 word9 word10 word11 word12`,
-        words: ["word9", "word10", "word11", "word12"],
+        content: `The foolish farmer’s wife also agreed and decided to use the function ‘cut’ on the goose’s stomach for the eggs. As soon as they killed the bird and opened the goose’s stomach, the output was nothing but guts and blood. The farmer, realizing his foolish mistake, cries over the bad result!`,
+        words: ["agreed", "function", "output", "result"],
       },
     ],
   },
   {
+    title: ``,
     part: [
       {
         content: `Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4Story 2 part 1 word1 word2 word3 word4v Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word3 word4 Story 2 part 1 word1 `,
@@ -776,6 +774,7 @@ export const story = [
     ],
   },
   {
+    title: ``,
     part: [
       {
         content: `Story 3 part 1 word1 word2 word3 word4`,
@@ -796,6 +795,7 @@ export const story = [
     ],
   },
   {
+    title: ``,
     part: [
       {
         content: `Story 4 part 1 word1 word2 word3 word4`,
@@ -812,6 +812,7 @@ export const story = [
     ],
   },
   {
+    title: ``,
     part: [
       {
         content: `Story 5 part 1 word1 word2 word3 word4`,
