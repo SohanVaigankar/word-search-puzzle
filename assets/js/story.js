0752808538dd86925ea810116de5d089558227eb
Sujit Maiti <skmaiti2050@gmail.com>
2021-07-18 21:55:02 +0530

    fix: add new story and cleanup

--- assets/js/story.js
+++ assets/js/story.js
@@ -1,95 +1,36 @@
 let story = [
   {
-    title: `The Golden  Egg`,
+    title: `The Golden Egg`,
     part: [
       {
-        content: `The computer reads us a story.. Once upon a time, a farmer had a goose that gives output as a golden egg every day. The egg provided enough money for the farmer and his wife for their day-to-day needs. The farmer and his wife were happy for a long time. But one day, the loop break. The farmer got an idea and thought, “Why should I take just one egg a day? Why can’t I execute the laying egg’s process at once and make a lot of money?”`,
+        content: `The computer reads us a story. Once upon a time, a farmer had a goose that gives output as a golden egg every day. The egg provided enough money for the farmer and his wife for their day-to-day needs. The farmer and his wife were happy for a long time. But one day, the loop break. The farmer got an idea and thought, “Why should I take just one egg a day? Why can’t I execute the laying egg’s process at once and make a lot of money?”`,
         words: ["computer", "output", "loop", "break", "execute", "process"],
       },
       {
         content: `The foolish farmer’s wife also agreed and decided to use the function ‘cut’ on the goose’s stomach for the eggs. As soon as they killed the bird and opened the goose’s stomach, the output was nothing but guts and blood. The farmer, realizing his foolish mistake, cries over the bad result!`,
         words: ["agreed", "function", "output", "result"],
-      },
-    ],
+      }
+    ]
   },
   {
-    title: ``,
+    title: `Story of different computers`,
     part: [
       {
-        content: `Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4Story 2 part 1 word1 word2 word3 word4v Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word3 word4 Story 2 part 1 word1 `,
-        words: ["word1", "word2", "word3", "word4"],
+        content: `Computers are electronic devices that can store, transmit and manipulate data and information. To process data it requires instructions to be given. The instructions are input through an input device called a keyboard, which is then processed, and the output is viewed on an output device such as a monitor.
+        These devices do not have emotions or feelings.`,
+        words: ["computer", "process", "input", "keyboard", "output", "monitor", "emotions", "feelings"],
       },
       {
-        content: `Story 2 part 2 word5 word6 word7 word8 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4Story 2 part 1 word1 word2 word3 word4v Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1`,
-        words: ["word5", "word6", "word7", "word8"],
+        content: `The different types of computers classified based on the sizes are as follows:
+        a) Super Computer: – It is mainly used for scientific purpose to perform large mathematical calculations. The first super computer was developed in India by CDAC (Center for Development and Advanced Computing) called Param 2000. The biggest Super Computer Division in Asia is at IISc, Bangalore (Indian Institute of Science).
+        b) Mainframe Computer: – It is used to process large volumes of data. Mainly used by commercial establishments such as banks and other financial organisations.`,
+        words: ["perform"],
       },
       {
-        content: `Story 2 part 3 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4Story 2 part 1 word1 word2 word3 word4v Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word9 word10 word11 word12`,
-        words: ["word9", "word10", "word11", "word12"],
-      },
-      {
-        content: `Story 2 part 4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4Story 2 part 1 word1 word2 word3 word4v Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word13 word14 word15 word16`,
-        words: ["word13", "word14", "word15", "word16"],
-      },
-      {
-        content: `Story 2 part 5 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4Story 2 part 1 word1 word2 word3 word4v Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word1 word2 word3 word4 Story 2 part 1 word17 word18 word19 word20`,
-        words: ["word17", "word18", "word19", "word20"],
-      },
-    ],
-  },
-  {
-    title: ``,
-    part: [
-      {
-        content: `Story 3 part 1 word1 word2 word3 word4`,
-        words: ["word1", "word2", "word3", "word4"],
-      },
-      {
-        content: `Story 3 part 2 word5 word6 word7 word8`,
-        words: ["word5", "word6", "word7", "word8"],
-      },
-      {
-        content: `Story 3 part 3 word9 word10 word11 word12`,
-        words: ["word9", "word10", "word11", "word12"],
-      },
-      {
-        content: `Story 3 part 4 word13 word14 word15 word16`,
-        words: ["word13", "word14", "word15", "word16"],
-      },
-    ],
-  },
-  {
-    title: ``,
-    part: [
-      {
-        content: `Story 4 part 1 word1 word2 word3 word4`,
-        words: ["word1", "word2", "word3", "word4"],
-      },
-      {
-        content: `Story 4 part 2 word5 word6 word7 word8`,
-        words: ["word5", "word6", "word7", "word8"],
-      },
-      {
-        content: `Story 4 part 3 word9 word10 word11 word12`,
-        words: ["word9", "word10", "word11", "word12"],
-      },
-    ],
-  },
-  {
-    title: ``,
-    part: [
-      {
-        content: `Story 5 part 1 word1 word2 word3 word4`,
-        words: ["word1", "word2", "word3", "word4"],
-      },
-      {
-        content: `Story 5 part 2 word5 word6 word7 word8`,
-        words: ["word5", "word6", "word7", "word8"],
-      },
-      {
-        content: `Story 5 part 3 word9 word10 word11 word12`,
-        words: ["word9", "word10", "word11", "word12"],
-      },
-    ],
-  },
+        content: `c) Mini Computers: – It is used to process medium volume of data. This can be used for networking (LAN and WAN).
+        d) Micro Computers: – Also called as personal computers. It is very widely used by individuals. It is used to process low volume of data. This can be used as a standalone computer and also in the network environment.`,
+        words: ["process", "network", "environment"],
+      }
+    ]
+  }
 ];
